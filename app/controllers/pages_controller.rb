16549db8563bfa5283632a080aed5dd9ee739894
jasonmag <jasonmaglangit@gmail.com>
2024-08-16 23:14:27 +0800

    added devise users and admin controller models; added design from tailwind templates;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
 
   def home
-    @resource = User.new  # or `current_user` if you want to pass the current logged-in user
-    @resource_name = :user
-    @devise_mapping = Devise.mappings[:user]
   end
 
   def about
