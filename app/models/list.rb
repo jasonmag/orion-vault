55ff07cc4ad85f817a1b95a0ca269043a02040b5
jasonmag <jasonmaglangit@gmail.com>
2024-09-07 16:54:08 +0800

    added create validation for null on effective start date

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,17 @@ class List < ApplicationRecord
   belongs_to :user
 
   include OrderableByDate
+  include EffectiveDateable
 
   # specify the column orderBy
   orderable_by_date :due_date
+
+  # Set default effective_start_date to today if not provided
+  before_validation :set_default_effective_start_date
+
+  private
+
+  def set_default_effective_start_date
+    self.effective_start_date ||= Date.today
+  end
 end
