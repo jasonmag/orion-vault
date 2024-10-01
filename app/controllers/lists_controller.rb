0123e2a65b37cfc4b58cb1d4f782c297ebea3173
jasonmag <jasonmaglangit@gmail.com>
2024-10-01 22:54:58 +0800

    remove start_date and end_date initialization; letting date_selectable handle the value

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,9 +6,6 @@ class ListsController < ApplicationController
 
   # GET /lists or /lists.json
   def index
-    @start_date = params[:start_date].present? ? Date.parse(params[:start_date]) : Date.today - 15.days
-    @end_date = params[:end_date].present? ? Date.parse(params[:end_date]) : Date.today + 15.days
-
     @lists_with_due_dates = current_user.list.visible_or_once(@start_date, @end_date).flat_map do |list|
       due_dates = list.due_dates_within_range(@start_date, @end_date)
       due_dates.map { |due_date| { list: list, due_date: due_date } }
