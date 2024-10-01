7f6fd56d143eea9f504dd428b43a0b0ca822edca
jasonmag <jasonmaglangit@gmail.com>
2024-10-01 22:54:15 +0800

    updated date_selectable start_date and end_date variables

--- app/controllers/concerns/date_selectable.rb
+++ app/controllers/concerns/date_selectable.rb
@@ -7,13 +7,25 @@ module DateSelectable
   end
 
   def select_dates
-    # This action will render the view to select start_date and end_date
+    # Render the view to select start_date and end_date
   end
 
   private
 
+  # Sets the @start_date and @end_date, using params, session, or default values
   def set_dates
-    @start_date = params[:start_date]
-    @end_date = params[:end_date]
+    if params[:start_date].present? && params[:end_date].present?
+      # If params are passed, parse them to Date objects and update the session
+      @start_date = session[:start_date] = Date.parse(params[:start_date])
+      @end_date = session[:end_date] = Date.parse(params[:end_date])
+    elsif session[:start_date].present? && session[:end_date].present?
+      # If no params but session values exist, ensure they are Date objects
+      @start_date = session[:start_date].to_date
+      @end_date = session[:end_date].to_date
+    else
+      # If no params or session, use the default date range
+      @start_date = Date.today
+      @end_date = Date.today + 15.days
+    end
   end
 end
