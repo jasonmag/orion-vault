ef0f15082d55fac1b3591bf1c19deac3e5d861a5
jasonmag <jasonmaglangit@gmail.com>
2024-10-28 22:46:10 +0800

    updated user setting default data

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_one :user_setting, dependent: :destroy
   accepts_nested_attributes_for :user_setting
   has_many :list
+
+  after_create :create_default_user_setting
+
+  private
+
+  def create_default_user_setting
+    create_user_setting(
+      default_date_range_list_display: "7",
+      default_currency: "USD",
+      notification_lead_time: 3
+    )
+  end
 end
--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -4,4 +4,13 @@ class UserSetting < ApplicationRecord
   # Add validations here if needed
   validates :default_date_range_list_display, presence: true
   validates :default_currency, presence: true
+
+  # Set default currency when empty
+  before_validation :set_default_currency
+
+  private
+
+  def set_default_currency
+    self.default_currency = "USD"
+  end
 end
