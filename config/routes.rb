0678987980f299ea1ebff444bafa0e3436309f7a
jasonmag <jasonmaglangit@gmail.com>
2025-12-19 00:48:45 +0800

    updated about page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  get "about" => "pages#about"
+
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
