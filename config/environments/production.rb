90852f658759fb1c9a98aee7e0ed054e9a14b795
jasonmag <jasonmaglangit@gmail.com>
2026-01-05 23:58:47 +0800

    updated hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "nexduev2.jasonmag.com"
+    "nexdue.jasonmag.com"
   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
